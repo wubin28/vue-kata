3c14c044289e3f27d2e47a815ca3b63375f470b8
Ben Wu <wubin28@gmail.com>
2023-09-27 22:12:02 +0800

    feat: display 10 numbers on the page using a v-for loop

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ const app = Vue.createApp({
     template: `
         <button v-on:click="increment">Increment</button>
         <p>{{ count }}</p>
+        
+        <div v-for="number in numbers">
+          <div> {{ number }} </div>
+        </div>
+        
         <div v-if="isEven()">Even</div>
         <div v-else>Odd</div>
     `,
 
     data() {
         return {
-            count: 0
+            count: 0,
+            numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         }
     },
 
